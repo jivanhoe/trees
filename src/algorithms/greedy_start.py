Bug fix: objective sign corrected for minimization problem
Other changes:
- Docstrings added to LocalSearch and GreedyStart classes
- Minor bug fix in Tree class where method refactor had not been done properly

src/algorithms/greedy_start.py
@@ -10,22 +10,41 @@ class GreedyStart:
 
     def __init__(
             self,
-            loss_criterion: Callable,
+            criterion: Callable,
             min_leaf_size: int = 1,
             max_depth: int = 10,
             max_features: Optional[float] = None,
-            max_examples: Optional[float] = None,
+            max_samples: Optional[float] = None,
             random_state: int = 0
     ):
-        self.loss_criterion = loss_criterion
+        """
+        Initialize a greedy search object used to a build feasible tree.
+        :param criterion: a callable object that accepts the arguments 'tree', a Tree object, and 'targets', a
+        numpy array of shape (n_samples,), and returns a float representing the objective the tree on the data
+        :param min_leaf_size: an integer hyperparameter specifying the minimum number of training examples in any leaf
+        :param max_depth:  an integer hyperparameter specifying the maximum depth of any leaf
+        :param max_features: a float hyperparameter in range (0, 1] the specifies the proportion of features considered
+        for each greedy split
+        :param max_samples: a float hyperparameter in range (0, 1] that specifies the proportion of samples from the
+        training data used to inform the greedy splits when growing the tree
+        :param random_state: an integer used to set the numpy seed to control the random behavior of the algorithm
+        """
+        self.criterion = criterion
         self.min_leaf_size = min_leaf_size
         self.max_depth = max_depth
         self.max_features = max_features
-        self.max_examples = max_examples
+        self.max_samples = max_samples
         self.random_state = random_state
         self.tree = Tree()
 
-    def _make_greedy_split(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarry) -> None:
+    def _make_greedy_split(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarray) -> None:
+        """
+        Blah
+        :param subtree: a Tree object
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: None
+        """
 
         # Initialize loss and best split parameters
         min_loss, best_split_feature, best_split_threshold = np.inf, None, None
@@ -54,14 +73,21 @@ class GreedyStart:
                 if self.min_leaf_size <= min([leaf_data.sum() for leaf_data in subtree.get_leaf_data()]):
 
                     # Check if split improves objective and update best split if so
-                    loss = self.loss_criterion(subtree=subtree, targets=targets)
+                    loss = -self.criterion(tree=subtree, targets=targets)
                     if loss < min_loss:
                         min_loss, best_split_feature, best_split_threshold = loss, split_feature, split_threshold
 
         # Reset root split to best split
         subtree.update_splits(inputs=inputs, split_feature=best_split_feature, split_threshold=best_split_threshold)
 
-    def _grow(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarry) -> None:
+    def _grow(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarray) -> None:
+        """
+        Recursively grow a tree using greedy splits until max depth or min leaf size is reached.
+        :param subtree: a Tree object
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: None
+        """
 
         # Check if subtree is leaf
         if subtree.is_leaf():
@@ -75,7 +101,13 @@ class GreedyStart:
                 if (child.data.sum() > self.min_leaf_size) and (child.root_depth < self.max_depth):
                     self._grow(subtree=child, inputs=inputs, targets=targets)
 
-    def build_tree(self, inputs: np.ndarray, targets: np.ndarry) -> Tree:
+    def build_tree(self, inputs: np.ndarray, targets: np.ndarray) -> Tree:
+        """
+        Build a feasible (and sensible) tree using a greedy splitting heuristic.
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: a Tree object that is feasible for the given data and hyperparameters
+        """
 
         # Set seed
         np.random.seed(self.random_state)
@@ -85,9 +117,9 @@ class GreedyStart:
             self.max_features = 1 / np.sqrt(inputs.shape[1])
 
         # If max examples is specified, select a stratified sample of training data
-        if self.max_examples:
+        if self.max_samples:
             inputs, targets, _, _ = train_test_split(inputs, targets, shuffle=True, stratify=targets,
-                                                     test_size=1-self.max_examples)
+                                                     test_size=1-self.max_samples)
 
         # Initialize tree data
         self.tree.data = np.ones(inputs.shape[0], dtype=bool)

src/algorithms/local_search.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from models.tree import Tree
 from algorithms.greedy_start import GreedyStart
-from metrics.classification_metrics import weighed_gini_impurity
+from metrics.classification_metrics import weighted_gini_impurity
 from metrics.regression_metrics import mean_squared_error
 
 
@@ -13,7 +13,7 @@ class LocalSearch:
 
     def __init__(
             self,
-            loss_criterion: Callable,
+            criterion: Callable,
             is_classifier: bool,
             tree: Optional[Tree] = None,
             min_leaf_size: int = 1,
@@ -22,7 +22,23 @@ class LocalSearch:
             tol: float = 1e-5,
             random_state: int = 0
     ):
-        self.loss_criterion = loss_criterion
+        """
+
+        :param criterion: a callable object that accepts the arguments 'tree', a Tree object, and 'targets', a
+        numpy array of shape (n_samples,), and returns a float representing the loss the tree on the data
+        :param is_classifier: a boolean that specifies if the tree is being used for a classification  problem, else it
+        is assumed to be for a regression problem
+        :param tree: an optional Tree object, which if provided is used as the feasible incumbent solution upon which
+        the local search begins improving (default None)
+        :param min_leaf_size: an integer hyperparameter that specifies the minimum number of training examples in any
+        leaf (default 1)
+        :param max_depth: an integer hyperparameter the specifies the maximum depth of any leaf (default 10)
+        :param complexity_param: a float hyperparameter that controls the trade-off between the ability of the tree to
+        fit the training data and its
+        :param tol: a
+        :param random_state: an integer used to set the numpy seed to control the random behavior of the algorithm
+        """
+        self.criterion = criterion
         self.is_classifier = is_classifier
         self.tree = tree
         self.min_leaf_size = min_leaf_size
@@ -32,9 +48,22 @@ class LocalSearch:
         self.random_state = random_state
 
     def _regularized_loss(self, subtree: Tree, targets: np.ndarray) -> float:
-        return self.loss_criterion(tree=subtree, targets=targets) + self.complexity_param * subtree.get_node_count()
+        """
+        Compute the regularized loss of a subtree based on the loss criterion and subtree complexity (i.e. node count).
+        :param subtree: a Tree object
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: a float representing the regularized loss of the subtree
+        """
+        return -self.criterion(tree=subtree, targets=targets) + self.complexity_param * subtree.get_node_count()
 
     def _optimize_subtree_root_split(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarray) -> float:
+        """
+        Optimize the split of the root node of a subtree using a brute-force approach.
+        :param subtree: a Tree object
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: a float representing the regularized loss of the subtree with the optimal root split
+        """
 
         # Initialize loss and best split parameters
         min_loss = self._regularized_loss(subtree=subtree, targets=targets)
@@ -44,7 +73,7 @@ class LocalSearch:
         if subtree.is_leaf():
             subtree.left, subtree.right = Tree(), Tree()
 
-        for split_feature in inputs.shape[1]:
+        for split_feature in range(inputs.shape[1]):
 
             # Get reference values for candidate split thresholds for feature
             values = inputs[subtree.data, split_feature].flatten()
@@ -73,6 +102,13 @@ class LocalSearch:
         return min_loss
 
     def _optimize_subtree_root_node(self, subtree: Tree, inputs: np.ndarray, targets: np.ndarray) -> float:
+        """
+        Optimize a subtree by modifying or deleting the root node split.
+        :param subtree: a Tree object
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: a float representing the regularized loss of the subtree with the optimized root node
+        """
 
         # Copy children
         left, right = map(deepcopy, subtree.get_children())
@@ -85,25 +121,33 @@ class LocalSearch:
 
         # Check if root deletion improves objective and replace with child if so
         for child in (left, right):
-            child.data = subtree.data
-            child.update_splits(inputs=inputs)
-            loss = self._regularized_loss(subtree=child, targets=targets)
-            if loss < min_loss:
-                min_loss = loss
-                subtree.left, subtree.right = child.left, child.right
-                subtree.update_splits(inputs=inputs, split_feature=child.split_feature,
-                                      split_threshold=child.split_thresold)
+            if child:
+                child.data = subtree.data
+                child.update_splits(inputs=inputs)
+                loss = self._regularized_loss(subtree=child, targets=targets)
+                if loss < min_loss:
+                    min_loss = loss
+                    subtree.left, subtree.right = child.left, child.right
+                    subtree.update_splits(inputs=inputs, split_feature=child.split_feature,
+                                          split_threshold=child.split_threshold)
         return min_loss
 
     def optimize_tree(self, inputs: np.ndarray, targets: np.ndarray) -> Tree:
+        """
+        Use a local search heuristic to iteratively improve a feasible tree until a local optimum is reached.
+        :param inputs: a numpy array of shape (n_samples, n_features) specifying the input values of the data
+        :param targets: a numpy array of shape (n_samples,) specifying the target values of the data
+        :return: a Tree object that is locally optimal for the given data
+        """
 
         # Set seed
         np.random.seed(self.random_state)
 
         # Initialize tree
         if self.tree is None:
-            greedy_start = GreedyStart(loss_criterion=(weighed_gini_impurity if self.is_classifier else mean_squared_error),
-                                       min_leaf_size=self.min_leaf_size, max_depth=self.max_depth, random_state=self.random_state)
+            greedy_start = GreedyStart(criterion=(weighted_gini_impurity if self.is_classifier else mean_squared_error),
+                                       min_leaf_size=self.min_leaf_size, max_depth=self.max_depth,
+                                       random_state=self.random_state)
             self.tree = greedy_start.build_tree(inputs=inputs, targets=targets)
         self.tree.data = np.ones(inputs.shape[0], dtype=bool)
         self.tree.update_splits(inputs=inputs)

src/metrics/classification_metrics.py
@@ -7,8 +7,8 @@ from models.tree import Tree
 
 
 def predict_probs(tree: Tree, targets: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    classes = np.unique(targets)
     targets = targets[tree.data]
+    classes = np.unique(targets)
     probs = np.zeros((targets.shape[0], classes.shape[0]))
     for leaf_data in tree.get_leaf_data():
         leaf_data = leaf_data[tree.data]
@@ -26,17 +26,21 @@ def roc_auc(tree: Tree, targets: np.ndarray) -> float:
     targets, probs = predict_probs(tree=tree, targets=targets)
     if probs.shape[1] > 2:
         return roc_auc_score(y_true=targets, y_score=probs, multi_class="ovr", average="macro")
-    return roc_auc_score(y_true=targets, y_score=probs[:, 1])
+    if probs.shape[1] == 2:
+        return roc_auc_score(y_true=targets, y_score=probs[:, 1])
+    return 1.0
 
 
 def average_precision(tree: Tree, targets: np.ndarray) -> float:
     targets, probs = predict_probs(tree=tree, targets=targets)
     if probs.shape[1] > 2:
         raise NotImplementedError
-    return average_precision_score(y_true=targets, y_score=probs)
+    if probs.shape[1] == 2:
+        return average_precision_score(y_true=targets, y_score=probs[:, 1])
+    return 1.0
 
 
-def weighed_gini_impurity(tree: Tree, targets: np.ndarray) -> float:
+def weighted_gini_impurity(tree: Tree, targets: np.ndarray) -> float:
     targets, probs = predict_probs(tree=tree, targets=targets)
     return (1 - (probs * probs).sum(1)).mean()
 

src/models/tree.py
@@ -48,8 +48,8 @@ class Tree:
             subtrees = []
         subtrees.append(self)
         if not self.is_leaf():
-            self.left.nodes(subtrees=subtrees)
-            self.right.nodes(subtrees=subtrees)
+            self.left.get_subtrees(subtrees=subtrees)
+            self.right.get_subtrees(subtrees=subtrees)
         return subtrees
 
     def get_leaf_data(self, leaf_data: Optional[List] = None) -> List[np.ndarray]:
@@ -58,8 +58,8 @@ class Tree:
         if self.is_leaf():
             leaf_data.append(self.data)
         else:
-            self.left.leaf_data(data=leaf_data)
-            self.right.leaf_data(data=leaf_data)
+            self.left.get_leaf_data(leaf_data=leaf_data)
+            self.right.get_leaf_data(leaf_data=leaf_data)
         return leaf_data
 
     def update_splits(
@@ -80,8 +80,9 @@ class Tree:
 
     def update_depth(self) -> None:
         for child in self.get_children():
-            child.root_depth = self.root_depth + 1
-            child.update_depth()
+            if child:
+                child.root_depth = self.root_depth + 1
+                child.update_depth()
 
 
 